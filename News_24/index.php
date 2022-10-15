22050f538c50a10447ee27497b08242c838a7b4d
abhi192809 <90956972+abhi192809@users.noreply.github.com>
2022-10-15 11:02:02 +0530

    Add files via upload

---
+++ News_24/index.php
@@ -0,0 +1,262 @@
+<?php include("header.php")?>
+<br>
+<div class="container">
+    <div class="row top_1">
+        <h1 style="color:red;">TOP <span style="color:gold;">News </span></h2>
+    </div>
+    <div class="row p-2">
+        <div class="col-md-6 border">
+            <h3>Todays News</h3>
+            <p><img class="w-100" src="img/mallikarjun-kharge-759.jpeg" alt=""></p>
+            <h4>Headline</h4>
+            <p>Mallikarjun Kharge, the frontrunner in the Congress leadership race, said on Thursday that he may not
+                consult the Nehru-Gandhi family on every decision, but would seek their “guidance” and “suggestions”,
+                pointing out that Sonia and Rahul Gandhi have experience helming the party.<a class="read_1" href="">Read More</a></p>
+        </div>
+        <div class="col-md-6 border">
+            <div class="container-fluid p-0 ">
+                <div class="row border">
+                    <div class="col-md-8">
+                        <h4>Headline</h4>
+                        <p>
+                            The Supreme Court on Thursday (October 13) delivered a split verdict in the Karnataka with one.
+                            <a class="read_1" href="">Read More</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 p-2">
+                        <p>
+                            <img class="w-100" src="img/supreme-court.jpeg" alt="">
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid p-0 ">
+                <div class="row border">
+                    <div class="col-md-8">
+                        <h4>Headline</h4>
+                        <p>
+                            The Supreme Court on Thursday (October 13) delivered a split verdict in the Karnataka with one.
+                            <a class="read_1" href="">Read More</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 p-2">
+                        <p>
+                            <img class="w-100" src="img/supreme-court.jpeg" alt="">
+                        </p>
+                    </div>
+                </div>
+                <div class="container-fluid p-0 ">
+                    <div class="row border">
+                        <div class="col-md-8">
+                            <h4>Headline</h4>
+                            <p>
+                                The Supreme Court on Thursday (October 13) delivered a split verdict in the Karnataka
+                                with one. <a class="read_1" href="">Read More</a>
+                               
+                            </p>
+                        </div>
+                        <div class="col-md-4 p-2">
+                            <p>
+                                <img class="w-100" src="img/supreme-court.jpeg" alt="">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid p-0 ">
+                    <div class="row border">
+                        <div class="col-md-8">
+                            <h4>Headline</h4>
+                            <p>
+                                The Supreme Court on Thursday (October 13) delivered a split verdict in the Karnataka
+                                 with one.
+                                <a class="read_1" href="">Read More</a>
+                            </p>
+                        </div>
+                        <div class="col-md-4 p-2">
+                            <p>
+                                <img class="w-100" src="img/supreme-court.jpeg" alt="">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container" style="font-family: sans-serif;">
+    <div class="row">
+        <h2 class="set_order">Top News</h2>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has taken
+                a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has taken
+                a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has taken
+                a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has taken
+                a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family </p>
+        </div>
+    </div>
+</div>
+<br>
+<div class="container" style="font-family:sans-serif;">
+    <div class="row">
+        <h2 class="set_order">Sport</h2>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+<div class="container" style="font-family:sans-serif;">
+    <div class="row">
+        <h2 class="set_order">NorthEast</h2>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+<div class="container" style="font-family:sans-serif;">
+    <div class="row">
+        <h2 class="set_order">India</h2>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+
+    </div>
+</div>
+<br>
+<div class="container" style="font-family:sans-serif;">
+    <div class="row">
+        <h2 class="set_order">World</h2>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+        <div class="col-md-3 top_news border text-center">
+            <p><img style="width:100%;" src="img/ND24_logo.png" alt=""></p>
+            <p class="just_fix">Bijni Witch-Hunting Case: 10 Sentenced To Life Imprisonment After 21 Years It has
+                taken a local court in Assam's Bijni nearly 21 years to find guilty 10 persons of killing a family
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+<script src="css/dark.js"></script>
+</body>
+
+</html>
